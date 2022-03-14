27d8ed8ceb3c3231fe63690af711a025b8eaa789
Yami-Kagayaki <lucasprogjr@gmail.com>
2022-03-14 02:16:27 -0300

    Aprendendo sobre Comentários

---
+++ Módulo 01 - Básico/02 Comentários/comentarios.js
@@ -0,0 +1,17 @@
+// Comentários que são ignorados
+
+//console.log('Olá')
+console.log("1");
+console.log("2");
+//console.log("3");
+console.log("4");
+
+// Comentário de linha
+
+/*
+Comentário 
+
+em 
+
+Bloco
+*/
