0d722d44ed1e0f2c4c7386d97879fa93c5bad30f
Yami-Kagayaki <lucasprogjr@gmail.com>
2022-03-14 02:16:08 -0300

    Aprendendo sobre o Console.log

---
+++ Módulo 01 - Básico/01 Consolelog/consolelog.js
@@ -0,0 +1,9 @@
+console.log("Volto da 'praia'"); //pode usar
+console.log(`Vou à praia`); //Pode usar
+console.log(52); //numeros
+console.log(5.2); //float
+console.log(5, 5); //Outros valores
+console.log(5 / 5); //Operadores
+
+console.log(52 + "anos"); //Concatenação
+console.log(52, "anos"); //Continuação de linha
---
+++ Módulo 01 - Básico/01 Consolelog/exercicio.js
@@ -0,0 +1,7 @@
+const name = "Lucas Gabriel";
+
+console.log(
+  '"Meu nome é"',
+  `"` + name + `"` + ".",
+  "Estou aprendendo JavaScript as 10 da manhã"
+);
