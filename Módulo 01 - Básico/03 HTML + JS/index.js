3858af07db7f01abcf5e7b43f055e81d1733b9a3
Yami-Kagayaki <lucasprogjr@gmail.com>
2022-03-15 15:38:21 -0300

    Aprendendo Integração com HTML

---
+++ Módulo 01 - Básico/03 HTML + JS/index.js
@@ -0,0 +1,6 @@
+console.log("Olá Mundo");
+
+//DOM - Document Object Model
+//Js para manipulação de DOM = Front-end
+//Js + Node para manipulação de um BD = Back-end
+//Node e navegador tem funções diferentes
