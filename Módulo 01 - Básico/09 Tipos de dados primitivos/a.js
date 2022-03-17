a58838b3d1ec66c98ed5288e6a655d794800a246
Yami-Kagayaki <lucasprogjr@gmail.com>
2022-03-17 16:42:06 -0300

    Aprendendo Tipos de dados Primitivos

---
+++ Módulo 01 - Básico/09 Tipos de dados primitivos/a.js
@@ -0,0 +1,13 @@
+const nome = "lucas"; //string
+const nome2 = `lucas`; //string
+const num = 50; //number
+const num1 = 50.0; //string
+const everdade = true; //boleano
+const everdade1 = false; //boleano (lógica)
+
+//Aqui temos que usar o let pois não podemos deixar uma var sem valor
+let indefinido; //undefined -> não aponta pra local nenhum na memória
+let nulo = null; // Nulo -> não aponta pra local nenhum na memória
+
+//Para ver i tipo de algo se usa o " typeof "
+console.log("Tipo de Indefinido é: ", typeof indefinido);
