8d5b35a444dbd816af3fe3da79e1eb01acb1285a
Yami-Kagayaki <lucasprogjr@gmail.com>
2022-05-26 16:30:06 -0300

    Aprendendo um pouco sobre o Math

---
+++ Módulo 01 - Básico/17 Math/a.js
@@ -0,0 +1,11 @@
+let num = 1.56161;
+let num2 = (4, 6, 7, 9, 1, 2);
+//math
+console.log(Math.floor(num)); //Arredonda para baixo
+console.log(Math.round(num)); //Arredonda para o mais próximo
+console.log(Math.ceil(num)); //Arredonda para cima
+
+console.log(Math.max(num2)); // pega o valor maior
+console.log(Math.min(num2)); // pega o valor menor
+
+console.log(Math.random() * (10 - 5) + 5);
