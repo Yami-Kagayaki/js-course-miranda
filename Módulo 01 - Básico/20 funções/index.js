16d22f490ee1c8f68858c4df7976769978875f43
Yami-Kagayaki <lucasprogjr@gmail.com>
2022-05-30 17:00:42 -0300

    Aprendendo um pouco sobre Funções

---
+++ Módulo 01 - Básico/20 funções/index.js
@@ -0,0 +1,23 @@
+//funçoes
+
+function minhafuncao(frase) {
+  console.log(frase);
+}
+
+minhafuncao("vasddasd");
+
+function log(vc) {
+  console.log(vc);
+}
+
+log("vc");
+///////////////////////////////////////////
+
+function soma(a, b) {
+  return a + b;
+}
+console.log(soma(5, 2));
+
+// Arrow function
+const raiz = (n) => n ** 0.5;
+console.log(raiz(9));
